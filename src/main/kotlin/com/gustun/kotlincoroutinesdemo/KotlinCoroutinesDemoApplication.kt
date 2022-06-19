b6cd8a9ca1fa75396b44071a7183dc599b228818
Gokcan Ustun <gokcanustun@Gokcans-MacBook-Pro.local>
2022-06-19 12:29:01 +0300

    initial commit

---
+++ src/main/kotlin/com/gustun/kotlincoroutinesdemo/KotlinCoroutinesDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.gustun.kotlincoroutinesdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinCoroutinesDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinCoroutinesDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/gustun/kotlincoroutinesdemo/KotlinCoroutinesDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gustun.kotlincoroutinesdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinCoroutinesDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
